a6920d5afd11d928ea387b9f276b2c8bd08d30db
Debajyoti Pal <paldebajyoti2000@gmail.com>
2026-06-02 10:38:13 +0530

    Time: 463 ms (51.35%) | Memory: 13.4 MB (0.00%) - LeetSync

---
+++ 3965-earliest-finish-time-for-land-and-water-rides-i/earliest-finish-time-for-land-and-water-rides-i.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def earliestFinishTime(self, landStartTime, landDuration, waterStartTime, waterDuration):
+        def ride(first, firsttime, second, secondtime):
+            k=[]
+            for i in range(len(first)):
+                for j in range (len(second)):
+                    if first[i]+firsttime[i]>=second[j]:
+                        c=first[i]+firsttime[i]+secondtime[j]
+                    else:
+                        c=second[j]+secondtime[j]
+                    k.append(c)
+            return k     
+        l1=ride(landStartTime, landDuration, waterStartTime, waterDuration)
+        l2=ride(waterStartTime, waterDuration, landStartTime, landDuration)
+        l=l1+l2
+        return min(l)
