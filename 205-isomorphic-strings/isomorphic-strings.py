5459ae6279577617d5f5967d2d51166059202e03
Debajyoti Pal <paldebajyoti2000@gmail.com>
2026-06-04 16:26:47 +0530

    Time: 13 ms (29.87%) | Memory: 15.2 MB (12.09%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def isIsomorphic(self, s, t):
+        m1={}
+        m2={}
+        for a,b in zip(s,t):
+            if a in m1 and m1[a]!=b:
+                return False
+            if b in m2 and m2[b]!=a:
+                return False
+            m1[a]=b
+            m2[b]=a
+        return True
+
