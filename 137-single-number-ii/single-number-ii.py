a16f655c805601359cee53d0b90d1c0312315c73
Debajyoti Pal <paldebajyoti2000@gmail.com>
2026-06-13 00:56:26 +0530

    Time: 3 ms (72.56%) | Memory: 20.9 MB (17.13%) - LeetSync

--- 137-single-number-ii/single-number-ii.py
+++ 137-single-number-ii/single-number-ii.py
@@ -1,8 +1,13 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        d={}
-        for i in set(nums):
-            d[nums.count(i)]=i
-        return d[1]
-
+        d = {}
+        for key in nums:
+            if key not in d:
+                d[key]=1
+            else:
+                d[key]+=1 
+        for key,value in d.items():
+            if value==1:
+                ans=key
+        return ans
         
