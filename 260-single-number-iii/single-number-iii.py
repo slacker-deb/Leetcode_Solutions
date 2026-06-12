7db279e4ec6aeda117660a47662f68b699aceecd
Debajyoti Pal <paldebajyoti2000@gmail.com>
2026-06-13 01:07:37 +0530

    Time: 3 ms (66.84%) | Memory: 20.9 MB (62.85%) - LeetSync

---
+++ 260-single-number-iii/single-number-iii.py
@@ -0,0 +1,15 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        d={}
+        ans=[]
+        for key in nums:
+            if key not in d:
+                d[key]=1
+            else:
+                d[key]+=1      
+        for key,value in d.items():
+            if value==1:
+                ans.append(key)
+        return ans
+    
+        
