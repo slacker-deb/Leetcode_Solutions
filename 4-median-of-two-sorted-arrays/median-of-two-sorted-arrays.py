6e07541c631520623747e9104157ca277ba7a7c6
Debajyoti Pal <paldebajyoti2000@gmail.com>
2026-06-13 01:14:04 +0530

    Time: 3 ms (47.78%) | Memory: 19.6 MB (15.31%) - LeetSync

---
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
@@ -0,0 +1,6 @@
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        from statistics import median
+        return median(nums1+nums2)
+
+        
